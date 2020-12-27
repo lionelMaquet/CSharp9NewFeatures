0592bf7a29599ce47363c9631ed1812c94f74016
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-27 19:41:40 +0100

    ...

---
+++ Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp9NewFeatures
+{
+    public class Person
+    {
+        // Third Feature : 
+        // the "init" means it can only be declared ONCE
+        public int YearOfBirth { get; init; } 
+        public string Name;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using CSharp9NewFeatures;
+using System;
+
+// First feature : 
+// Top level code ! 
+// For this type of apps, we don't need the namespace and class declaration. 
+// Only one class can have top level declarations
+// That class will be the entry point to the program.
+Console.WriteLine("Hello World!");
+
+
+// Second Feature : 
+// The type is inferred, we don't need to write it twice 
+// old way : Person person = new Person();
+// new way : 
+Person person = new() { YearOfBirth = 1995, Name = "John Doe" };
+
+// Third Feature : 
+person.Name = "Bob Doe"; // This is fine
+// person.YearOfBirth = 1998; // PROBLEM -> This property has {get;set;}, therefore, it can only be initialized and read, never set twice. 
+
+
+// Fourth Feature : new switch expression
+int temperature = new Random().Next(-50, 100);
+string temperatureDetails = temperature switch
+{
+    > -30 and < -15 => "Hardly livable",
+    < 0 => "Freezing",
+    0 => "Freezing point",
+    > 0 and < 15 => "Cool but fine", // <--- logical operator and, not &&. You could also say or
+    < 30 => "Fine",
+    < 45 => "Hot",
+    _ => "Not livable"
+};
+
+Console.WriteLine(temperature);
+Console.WriteLine(temperatureDetails);
+
+
+// Fifth feature : is not null
+Person albert = null;
+
+if (albert is not null)
+{
+    Console.WriteLine("Albert has value");
+}
+
+
+
